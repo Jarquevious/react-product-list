12f887ceeec571d457842f5f03a2917481374a6c
Make School Student Loaner <makeschoolloaner@Makes-MacBook-Pro.local>
2021-04-18 18:44:34 -0700

    productsjs and product filter component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
-import './data.js'
+import data from './data';
+import React, { useState } from 'react';
+import Product from './components/Product/Product'
+import ProductFilter from './components/ProductFilter/ProductFilter';
+
 
 function App() {
+  const [category, setCategory] = useState('All');
+
   return (
     <div className="App">
       <header className="App-header">
