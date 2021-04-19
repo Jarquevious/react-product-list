7519b9de03cf3ce558db5f296d031762cee3cc27
Make School Student Loaner <makeschoolloaner@Makes-MacBook-Pro.local>
2021-04-18 23:26:08 -0700

    changes made

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
-import './App.css';
 import data from './data';
-import React, { useState } from 'react';
-import Product from './src/components/Product'
-import ProductFilter from './src/components/ProductFilter';
+import React from 'react';
 
+import './App.css';
 
 function App() {
-  const [category, setCategory] = useState('All');
-
-  const display_category = categoriesUnique.map((item) => {
-    return (
-        <button
-            className="button-items"
-            onClick={() => {
-                setCategory(item)
-
-            }}
-        >{item}
-        </button>
-    )
-})
-
   return (
     <div className="App">
-      <button
-          className="button"
-          category={'All'}
-          onClick={() => 
-            { setCategory('All') }}
-            >All {data.length}
-     </button>
-     <Product category={category} setCategory={setCategory} />
-     <ProductFilter category={category} />
+      <div className="App-header">
+      </div>
+      <data />
     </div>
   );
 }
