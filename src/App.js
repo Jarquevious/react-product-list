139ac1b00385dd5f25dd66d62866f2a9cc358ce0
Jarquevious Nelson <jarqueviousnelson@Jarqueviouss-MacBook-Pro.local>
2021-04-23 16:29:12 -0700

    changes made

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import data, {  categoriesUnique } from './data'
-import React from 'react';
+import data, { categoriesUnique } from './data'
+import React, { useState } from 'react';
 // categoriesWithCounts,, productCount 
 import './App.css';
-  
+
+categoriesUnique.push('all')
 
 function App() {
+  const [category, setCategory] = useState('all')
+
   return (
     <div className="App">
+      <h1>{ category }</h1>
       <div className="App-header" >
-      { categoriesUnique.map((category) => {
-    
-    return <button>{category}</button>
-      })}
+        {categoriesUnique.map((category) => {
+          return <button
+                  className="category-button"
+                  onClick = { () => {
+                    setCategory(category)
+                  }}
+          >{category}</button>
+        })}
 
-      <div>
-        { data.map((data) => {
-          return (
-          <div>
-            <h1>{ data.name } </h1>
-            <h2>{ data.category }</h2>
-            <h3>{ data.price }</h3>
-
-          </div>
-        )})} 
+        <div className="Products">
+          {data.filter( (item) => {
+            return item.category === category || category === 'all'
+          }).map((data) => {
+            return (
+              <div className="product">
+                <h1>{data.name} </h1>
+                <h2>{data.category}</h2>
+                <h3>{data.price}</h3>
+              </div>
+            )
+          })}
+        </div>
       </div>
-      
-    
-        <button></button> 
-      </div>
-    
       <data />
     </div>
   );
