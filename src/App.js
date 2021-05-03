18076fa2215f4f67d4c5451753cfce103f771b64
Jarquevious Nelson <jarquevious@gmail.com>
2021-05-03 11:40:27 -0700

    shopping cart page added, still in progress

--- src/App.js
+++ src/App.js
@@ -1,61 +1,67 @@
-import data, { categoriesUnique, productCount} from './data'
-import React, { useState } from 'react';
-// categoriesWithCounts,,  
-import './App.css';
-import Card from 'react-bootstrap/Card'
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
+import data, { categoriesUnique, productCount } from "./data";
+import React, { useState } from "react";
+import "./App.css";
+import Card from "react-bootstrap/Card";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import shoppingCart from './shoppingCartPage'
 
 
-categoriesUnique.push('all')
+categoriesUnique.push("all");
 
 function App() {
-  
-  const [category, setCategory] = useState('all')
-  
+  const [category, setCategory] = useState("all");
 
   return (
-    
     <div className="App">
+      <Router>
+        <Link to="/">Shop</Link>
+        <Link to="/cart">Cart</Link>
+        <Route path="/cart" component={ShoppingCartPage} />
+        <Route path="/" exact component={ShoppingPage} />
+      </Router>
       <Navbar />
       <div className="App-header">
-        
-        {categoriesUnique.map(( category ) => {
+        {categoriesUnique.map((category) => {
           return (
-                <button
-                  size="sm"
-                  variant="secondary"
-                  className="category-button"
-                  key={productCount}
-                  onClick = { () => {
-                    setCategory(category)
-                  }}
-          >{category}</button>
-        )})}</div>
-        <div className="blue">
-          <h1>
-            { category }
-          </h1>
-        </div>
-          <div className="Products">
-            {data.filter( (item) => {
-              return item.category === category || category === 'all'
-            }).map((data) => {
-              return (
-                <div className="product">
-                  <Card className="Card">
-                    <h1>{data.name} </h1>
-                    <h2>{data.category}</h2>
-                    <h3>{data.price}</h3> 
-                  </Card>           
-                </div>
-            )
+            <button
+              size="sm"
+              variant="secondary"
+              className="category-button"
+              key={productCount}
+              onClick={() => {
+                setCategory(category);
+              }}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+      <div className="blue">
+        <h1>{category}</h1>
+      </div>
+      <div className="Products">
+        {data
+          .filter((item) => {
+            return item.category === category || category === "all";
+          })
+          .map((data) => {
+            return (
+              <div className="product">
+                <Card className="Card">
+                  <h1>{data.name} </h1>
+                  <h2>{data.category}</h2>
+                  <h3>{data.price}</h3>
+                </Card>
+              </div>
+            );
           })}
-          
-        </div>
-      
+      </div>
+
       <data />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
---
+++ src/ShoppingCartPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function shoppingCart() {
+   const 
+
+}
+
+export default shoppingCart
